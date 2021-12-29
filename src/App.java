b13edb686e7b12f3ca81c2bf3925025ade5d71c8
Hyojun Ahn <wnsgy79@gmail.com>
2021-12-29 22:09:03 +0900

    no

--- src/App.java
+++
@@ -1,9 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
-/*
- * 간단한 문자 출력
- * "Hello, Word!" 출력
- */
