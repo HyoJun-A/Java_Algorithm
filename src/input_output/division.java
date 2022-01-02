62ef527490b2f7d5917e85ecdff92d7d736fd870
Hyojun Ahn <wnsgy79@gmail.com>
2022-01-02 22:12:18 +0900

    I/O

---
+++ src/input_output/division.java
@@ -0,0 +1,13 @@
+package input_output;
+
+import java.util.Scanner;
+
+public class division {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double A = sc.nextDouble();
+        double B = sc.nextDouble();
+
+        System.out.println(A / B);
+    }
+}
