4322fa1ef0ca2c76778e75af45134c15c677ead4
Hyojun Ahn <wnsgy79@gmail.com>
2022-01-02 21:27:24 +0900

    I/O

---
+++ src/input_output/Minus.java
@@ -0,0 +1,15 @@
+package input_output;
+
+import java.util.Scanner;
+
+public class Minus {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        int C = A - B;
+        System.out.println(C);
+    }
+}
