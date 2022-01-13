735ad05f7d8b2a25cb8d0cd3944afc2812ba8c87
HyoJun-A <wnsgy79@gmail.com>
2022-01-13 15:32:00 +0900

    Input/Output

---
+++ src/input_output/multiply2.java
@@ -0,0 +1,23 @@
+package input_output;
+
+import java.util.Scanner;
+
+import javax.swing.plaf.synth.SynthStyle;
+
+public class multiply2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        int Hundred = (B % 10) * A;
+        int ten = ((B % 100) / 10) * A;
+        int one = (B / 100) * A;
+        int multi = A * B;
+
+        System.out.println(Hundred);
+        System.out.println(ten);
+        System.out.println(one);
+        System.out.println(multi);
+    }
+}
---
+++ src/input_output/rest.java
@@ -0,0 +1,17 @@
+package input_output;
+
+import java.util.Scanner;
+
+public class rest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+
+        System.out.println((A + B) % C);
+        System.out.println(((A % C) + (B % C)) % C);
+        System.out.println((A * B) % C);
+        System.out.println(((A % C) * (B % C)) % C);
+    }
+}
