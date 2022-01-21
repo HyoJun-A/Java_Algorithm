3c3fbdb3507cc9181b5c1ac1487dd55d190cfe08
HyoJun-A <wnsgy79@gmail.com>
2022-01-21 16:44:07 +0900

    For

--- src/For_/For10.java
+++ src/For_/For10.java
@@ -1,18 +1,22 @@
 package For_;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class For10 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int X = sc.nextInt();
+        int N = sc.nextInt(); // 수열 A의 총 갯수
+        int X = sc.nextInt(); // A의 값들과 비교할 X
 
+        // 수열 A를 넣기 위한 리스트
         int arr[] = new int[N];
+
+        // for문을 돌려 arr[]안에 수열 값들을 넣는다.
         for (int i = 0; i < N; ++i) {
             arr[i] = sc.nextInt();
         }
+
+        // arr[]을 X와 비교하여 arr[i] 가 X보다 작을경우 arr[i]출력
         for (int i = 0; i < N; ++i) {
             if (arr[i] < X) {
                 System.out.print(arr[i] + " ");
@@ -22,3 +26,4 @@ public class For10 {
         sc.close();
     }
 }
+// 정수 N개로 이루어진 수열 A와 정수 X가 주어진다. 이때, A에서 X보다 작은 수를 모두 출력하는 프로그램을 작성하시오.
