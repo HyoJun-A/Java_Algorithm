5a19e097dbd2f1a92051d67e46d136d2a883b88e
HyoJun-A <wnsgy79@gmail.com>
2022-02-03 14:37:33 +0900

    Array

--- src/Array_/Array4.java
+++ src/Array_/Array4.java
@@ -1,7 +1,6 @@
 package Array_;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Scanner;
@@ -10,20 +9,20 @@ import java.util.Set;
 public class Array4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        // 나머지를 구하기 위한 값은 무조건 10개가 들어간다고 선언되어 있기때문에 크기10의 배열을 생성 하여 나머지 값을 넣어줌
         int[] T = new int[10];
-
         for (int i = 0; i < 10; i++) {
             int A = sc.nextInt();
             T[i] = A % 42;
         }
 
+        // Set을 사용 하여 값을 넣어줌 , set은 중복을 포함하지 않기 때문에 사용
         Set<Integer> set = new HashSet<Integer>();
-
         for (int i : T)
             set.add(i);
 
+        // 리스트에 넣지 않더라도 정렬을 할수 있으며 다양하게 사용할수 있기 때문에 사용
         List<Integer> list = new ArrayList<Integer>(set);
-        Collections.sort(list);
 
         System.out.println(list.size());
         sc.close();
