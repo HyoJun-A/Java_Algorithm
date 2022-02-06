ad42a573aa3e8b0b5275f2e86fd8f9abeb164d37
HyoJun-A <wnsgy79@gmail.com>
2022-02-06 14:05:38 +0900

    Array

--- src/Array_/Array6.java
+++ src/Array_/Array6.java
@@ -1,8 +1,17 @@
 package Array_;
 
+import java.util.Scanner;
+
 public class Array6 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        String[] result = new String[T];
+        for (int i = 0; i < T; ++i) {
+            result[i] = sc.nextLine();
+        }
 
+        sc.close();
     }
 }
 
@@ -15,6 +24,7 @@ public class Array6 {
 // 입력
 // 첫째 줄에 테스트 케이스의 개수가 주어진다. 각 테스트 케이스는 한 줄로 이루어져 있고, 길이가 0보다 크고 80보다 작은 문자열이
 // 주어진다. 문자열은 O와 X만으로 이루어져 있다.
+
 
 // 출력
 // 각 테스트 케이스마다 점수를 출력한다.
